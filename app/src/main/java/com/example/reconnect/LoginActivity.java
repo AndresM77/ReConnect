138f81a867dd40518c30279f574a00d711c996bf
Andres Montoya <andresmontoya7@fb.com>
2019-07-15 15:51:09 -0700

    Implemented LoginActivity java class and added gradle Parse libraries

--- app/src/main/java/com/example/reconnect/LoginActivity.java
+++ app/src/main/java/com/example/reconnect/LoginActivity.java
@@ -1,14 +1,84 @@
 package com.example.reconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    //Referencing login objects
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // show the signup or login screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Persistence functionality
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            // do stuff with the user
+            final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+
+            usernameInput = findViewById(R.id.tvUserText);
+            passwordInput = findViewById(R.id.tvPassText);
+            loginBtn = findViewById(R.id.btnLogIn);
+            registerBtn = findViewById(R.id.btnSignUp);
+
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final String username = usernameInput.getText().toString();
+                    final String password = passwordInput.getText().toString();
+
+                    login(username, password);
+                }
+            });
+
+            registerBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
+    }
+
+    private void login (final String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null)
+                {
+                    Log.d("LoginActivity", "Login successful");
+
+                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.e("LoginActivity", "Login failure");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
+

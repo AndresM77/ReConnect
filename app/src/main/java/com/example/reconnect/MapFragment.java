c9cfa0e52a5d7b341518b1d3674dbcec266689da
Andres Montoya <andresmontoya7@fb.com>
2019-07-15 16:34:43 -0700

    Created map fragment java and xml files

--- app/src/main/java/com/example/reconnect/MapFragment.java
+++ app/src/main/java/com/example/reconnect/MapFragment.java
@@ -1,6 +1,29 @@
 package com.example.reconnect;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 class MapFragment extends Fragment {
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }

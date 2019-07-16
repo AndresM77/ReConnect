6d54c21d18d257c88277bec755dbf7b94755610a
camsim99 <csimon@cs.hmc.edu>
2019-07-16 13:32:59 -0700

    Created RequestMeeting's layout - Contact.java I deleted a while ago but just now being pushed - AndroidManifest.xml updated automatically

--- app/src/main/java/com/example/reconnect/RequestMeeting.java
+++ app/src/main/java/com/example/reconnect/RequestMeeting.java
@@ -3,8 +3,15 @@ package com.example.reconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RequestMeeting extends AppCompatActivity {
+
+    EditText requestee;
+    EditText date;
+    Button submitRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

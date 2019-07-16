db36ac1bffee92015500932bef9d8cd45e6ac79a
camsim99 <csimon@cs.hmc.edu>
2019-07-16 10:10:19 -0700

    Implemented Constructors for MapAdapter and its ViewHolder -Also implemented the onCreateViewHolder function

--- app/src/main/java/com/example/reconnect/MapAdapter.java
+++ app/src/main/java/com/example/reconnect/MapAdapter.java
@@ -1,27 +1,41 @@
 package com.example.reconnect;
 
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MapAdapter extends RecyclerView.Adapter<MapAdapter.ViewHolder> {
 
-    public MapAdapter() {
-        super();
+    private Context context;
+    private List<Contact> contacts;
+
+    public MapAdapter(Context context, List<Contact> contacts) {
+        this.context = context;
+        this.contacts = contacts;
     }
 
     @NonNull
     @Override
     public MapAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.item_contact, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MapAdapter.ViewHolder holder, int position) {
-
     }
+
+    private TextView name;
+    private TextView latitude;
+    private TextView longitude;
 
     @Override
     public int getItemCount() {
@@ -32,6 +46,11 @@ public class MapAdapter extends RecyclerView.Adapter<MapAdapter.ViewHolder> {
 
         public ViewHolder(View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.contactName);
+            latitude = itemView.findViewById(R.id.contactLatitude);
+            longitude = itemView.findViewById(R.id.contactLongitude);
         }
+
+
     }
 }

e0bd979db909849ea60cd01dc6e4030f29457aad
camsim99 <csimon@cs.hmc.edu>
2019-07-16 10:02:25 -0700

    Added Conctact Class - To be used to create the Contact listing on the map

---
+++ app/src/main/java/com/example/reconnect/Contact.java
@@ -0,0 +1,33 @@
+package com.example.reconnect;
+
+public class Contact {
+
+    //The three pieces of info shown in the contact list on the map
+    public String name;
+    public String latitude;
+    public String longitude;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}

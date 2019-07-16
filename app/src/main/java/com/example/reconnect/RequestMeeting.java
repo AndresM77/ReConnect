42aa95a8359376c1b47af9c6a8b7f644d19378c3
camsim99 <csimon@cs.hmc.edu>
2019-07-16 13:17:33 -0700

    Added RequestMeeting Activity - To prompt user to request meeting with selected User in contacts on Map

---
+++ app/src/main/java/com/example/reconnect/RequestMeeting.java
@@ -0,0 +1,14 @@
+package com.example.reconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestMeeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_meeting);
+    }
+}

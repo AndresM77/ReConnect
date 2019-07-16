9dd5ab001d763522d0d892befec240f951e557bf
camsim99 <csimon@cs.hmc.edu>
2019-07-16 14:38:44 -0700

    Edited RequestMeeting.java to account for all the Views in its layout folder

--- app/src/main/java/com/example/reconnect/RequestMeeting.java
+++ app/src/main/java/com/example/reconnect/RequestMeeting.java
@@ -2,20 +2,61 @@ package com.example.reconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.reconnect.fragments.CalendarFragment;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 public class RequestMeeting extends AppCompatActivity {
 
-    EditText requestee;
+    TextView requestee;
     EditText date;
+    EditText startTime;
+    EditText endTime;
     Button submitRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_meeting);
+
+        requestee = findViewById(R.id.requesteeName);
+        date = findViewById(R.id.requestDate);
+        startTime = findViewById(R.id.startTime);
+        endTime = findViewById(R.id.endTime);
+        submitRequest = findViewById(R.id.submitRequest);
+
+        // grab the objectId of the requested User
+        String requestedUserId = getIntent().getStringExtra("requesteeId");
+
+        // find the requested User in our Parse database
+        ParseQuery<ParseUser> userParseQuery = new ParseQuery<>(ParseUser.class);
+        try {
+            ParseUser requestedUser = userParseQuery.get(requestedUserId);
+            requestee.setText(requestedUser.getUsername());
+        }
+        catch(ParseException e) {
+            Log.e("RequestMeeting Activity", "Unable to get the name of the requested User!");
+            e.printStackTrace();
+        }
+
+        // onClick listener for the submit action
+        submitRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RequestMeeting.this, CalendarFragment.class);
+
+                // Need to pass the requested user, date,
+                //TODO make sure i have all things to make event properly
+            }
+        });
     }
 }

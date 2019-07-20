d19b0e6670bf16087c8c6a1ad98dd60eab363a91
Andres Montoya <andres@triplean.com>
2019-07-19 17:45:24 -0700

    Properly implemented camera and enabled profile photo uploading

--- app/src/main/java/com/example/reconnect/Activities/HomeActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/HomeActivity.java
@@ -104,6 +104,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == REQUEST_CODE) {
             Connection connection = data.getParcelableExtra("connection");
             createConversation(connection);
--- app/src/main/java/com/example/reconnect/fragments/CalendarFragment.java
+++ app/src/main/java/com/example/reconnect/fragments/CalendarFragment.java
@@ -1,6 +1,8 @@
 package com.example.reconnect.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,18 +27,19 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.bumptech.glide.Glide;
 import com.example.reconnect.Adapters.EventAdapter;
 import com.example.reconnect.R;
-import com.example.reconnect.model.DateTitle;
 import com.example.reconnect.model.Event;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class CalendarFragment extends Fragment {
 
@@ -52,7 +55,7 @@ public class CalendarFragment extends Fragment {
     // for changing profile picture
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     public String photoFileName = "photo.jpg";
-    private File photoFile;
+    public File photoFile;
     private ImageView ivProfilePic;
     private Button btnChangeProfile;
 
@@ -101,8 +104,6 @@ public class CalendarFragment extends Fragment {
                     Toast.makeText(getContext(), "There is no photo!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Glide.with(getContext()).load(photoFile).into(ivProfilePic);
-                saveUser(photoFile);
             }
         });
 
@@ -131,55 +132,71 @@ public class CalendarFragment extends Fragment {
     }
 
     public void launchCamera() {
-        Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        if (hasImageCaptureBug()) {
-            i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File("/sdcard/tmp")));
-        } else {
-            i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.example.reconnect", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
-        startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
 
-    public void saveUser (File photoFile) {
-        ParseFile photo = new ParseFile(photoFile);
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "ProfileActivity");
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("ProfileActivity", "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+
+    public void saveUser (final File photoFile) {
+        final ParseFile photo = new ParseFile(photoFile);
         ParseUser user = ParseUser.getCurrentUser();
         user.put("profileImg", photo);
-        user.saveInBackground();
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivProfilePic.setImageBitmap(takenImage);
+            }
+        });
     }
 
-    public boolean hasImageCaptureBug() {
-
-        // list of known devices that have the bug
-        ArrayList<String> devices = new ArrayList<String>();
-        devices.add("android-devphone1/dream_devphone/dream");
-        devices.add("generic/sdk/generic");
-        devices.add("vodafone/vfpioneer/sapphire");
-        devices.add("tmobile/kila/dream");
-        devices.add("verizon/voles/sholes");
-        devices.add("google_ion/google_ion/sapphire");
-
-        return devices.contains(android.os.Build.BRAND + "/" + android.os.Build.PRODUCT + "/"
-                + android.os.Build.DEVICE);
-
-    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        switch (requestCode) {
-            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
-                Uri u;
-                if (hasImageCaptureBug()) {
-                    File fi = new File("/sdcard/tmp");
-                    try {
-                        u = Uri.parse(android.provider.MediaStore.Images.Media.insertImage(getContext().getContentResolver(), fi.getAbsolutePath(), null, null));
-                        if (!fi.delete()) {
-                            Log.i("profileImg", "Failed to delete " + fi);
-                        }
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    u = intent.getData();
-                }
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                saveUser(photoFile);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 

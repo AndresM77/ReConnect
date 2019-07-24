ecdd50b5459f067bde3c756dfb3f74cbde880fba
Andres Montoya <andres@triplean.com>
2019-07-23 17:47:43 -0700

    Implemented methods which retrieve contacts from phone

--- app/src/main/java/com/example/reconnect/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/SettingsActivity.java
@@ -1,9 +1,14 @@
 package com.example.reconnect.Activities;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +17,7 @@ import com.parse.ParseUser;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final String TAG = "SettingsActivity";
     //Implementing Item view listeners
     private Button btnLogOut;
     private Button btnReturn;
@@ -24,6 +30,16 @@ public class SettingsActivity extends AppCompatActivity {
 
         btnLogOut = findViewById(R.id.btnLogOut);
         btnReturn = findViewById(R.id.btnReturn);
+        btnUpload = findViewById(R.id.btnUpload);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Unleash the best and get the contacts
+                getContactList();
+                Toast.makeText(getApplicationContext(), "Check logs", Toast.LENGTH_LONG).show();
+            }
+        });
 
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +57,39 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void getContactList() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+                if (cur.getInt(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Log.i(TAG, "Name: " + name);
+                        Log.i(TAG, "Phone Number: " + phoneNo);
+                    }
+                    pCur.close();
+                }
+            }
+        }
+        if(cur!=null){
+            cur.close();
+        }
     }
 }

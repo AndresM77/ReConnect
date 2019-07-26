92c9a6804a3aea6e298b19ca4056c230fa409485
Alisha Mirapuri <amirapuri@berkeley.edu>
2019-07-26 14:34:26 -0700

    Added toolbar to the top of Request Meetings activity so we can get rid of the "return" button. Also added settings button to toolbar.

--- app/src/main/java/com/example/reconnect/Activities/RequestMeetingActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/RequestMeetingActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -300,4 +302,34 @@ public class RequestMeetingActivity extends AppCompatActivity {
     public interface TimePickerDoneListener {
         void done(String timeText);
     }
+
+    // for toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_request, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.ivSettings:
+                //Take to settings activity
+                Intent i = new Intent(RequestMeetingActivity.this, SettingsActivity.class);
+                startActivity(i); // brings up the second activity
+                return true;
+            case R.id.ivBack:
+                //Take back, finish this intent
+                Intent intent = new Intent(RequestMeetingActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }

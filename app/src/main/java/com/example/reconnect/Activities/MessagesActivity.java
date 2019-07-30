9593d68107dea7d2b5e0fdfa9cb1abd5ffb97b67
camsim99 <csimon@cs.hmc.edu>
2019-07-30 09:38:33 -0700

    Moved return to action bar on messages

--- app/src/main/java/com/example/reconnect/Activities/MessagesActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/MessagesActivity.java
@@ -3,6 +3,8 @@ package com.example.reconnect.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -44,7 +47,6 @@ public class MessagesActivity extends AppCompatActivity {
     private ImageView ivProfileImage;
     private EditText etMessage;
     private Button btnSubmit;
-    private Button btnReturn;
     private Conversation conversation;
 
     @Override
@@ -68,7 +70,6 @@ public class MessagesActivity extends AppCompatActivity {
         tvContactName = findViewById(R.id.tvContactName);
         etMessage = findViewById(R.id.etMessage);
         btnSubmit = findViewById(R.id.btnMessage);
-        btnReturn = findViewById(R.id.btnReturn);
         tvDistanceAway = findViewById(R.id.tvDistanceAway);
         tvIndustry = findViewById(R.id.tvIndustry);
         ivProfileImage = findViewById(R.id.ivProfileImg);
@@ -94,14 +95,6 @@ public class MessagesActivity extends AppCompatActivity {
             }
         });
 
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MessagesActivity.this, HomeActivity.class);
-                startActivity(i);
-            }
-        });
-
         // onClick listener to request a meeting
         ivProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,6 +108,13 @@ public class MessagesActivity extends AppCompatActivity {
                 view.getContext().startActivity(intent);
             }
         });
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.messageToolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Lookup the swipe container view
         swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
@@ -168,6 +168,27 @@ public class MessagesActivity extends AppCompatActivity {
                 queryMessages(conversation);
             }
         });
+    }
+
+    // for toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void queryMessages(Conversation conversation) {

466d9a452524ed066ff1a51f000ddefada3688aa
Andres Montoya <andres@triplean.com>
2019-07-30 11:08:30 -0700

    Implemented queries for contacts and users for uploading contacts and got integer list of phone numbers

--- app/src/main/java/com/example/reconnect/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/SettingsActivity.java
@@ -13,7 +13,14 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.reconnect.R;
+import com.example.reconnect.model.Connection;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -24,6 +31,10 @@ public class SettingsActivity extends AppCompatActivity {
     private Button btnReturn;
     private Button btnUpload;
     private Button btnGetContacts;
+    private List<ParseUser> mUsers;
+    private List<Connection> mConnections;
+    private List<String> cUserNames;
+    private List<Integer> mPhones;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,10 @@ public class SettingsActivity extends AppCompatActivity {
         btnReturn = findViewById(R.id.btnReturn);
         btnUpload = findViewById(R.id.btnUpload);
         btnGetContacts = findViewById(R.id.btnGetContacts);
+        mUsers = new ArrayList<>();
+        mPhones = new ArrayList<>();
+        mConnections = new ArrayList<>();
+        cUserNames = new ArrayList<>();
 
         btnGetContacts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +63,8 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //Unleash the best and get the contacts
                 getContactList();
+                //query users
+                queryUsers();
                 Toast.makeText(getApplicationContext(), "Check logs", Toast.LENGTH_LONG).show();
             }
         });
@@ -68,9 +85,14 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+        //query connections
+        queryConnections();
     }
 
     private void getContactList() {
+        mPhones.clear();
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
@@ -90,10 +112,16 @@ public class SettingsActivity extends AppCompatActivity {
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                             new String[]{id}, null);
                     while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                         Log.i(TAG, "Name: " + name);
                         Log.i(TAG, "Phone Number: " + phoneNo);
+                        int areaCodeFactor = 10000000;
+                        int secondCodeFactor = 10000;
+                        int placeholder = Integer.parseInt(phoneNo.substring(0,2)) * areaCodeFactor;
+                        placeholder += Integer.parseInt(phoneNo.substring(3,6)) * secondCodeFactor;
+                        placeholder += Integer.parseInt(phoneNo.substring(7));
+                        Log.i(TAG, "Phone Number in int: " + placeholder);
+                        mPhones.add(placeholder);
                     }
                     pCur.close();
                 }
@@ -101,6 +129,70 @@ public class SettingsActivity extends AppCompatActivity {
         }
         if(cur!=null){
             cur.close();
+        }
+        checkForConnections();
+    }
+
+    private void queryConnections() {
+        Connection.queryConnections(new FindCallback<Connection>() {
+            @Override
+            public void done(List<Connection> objects, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error with Query");
+                    e.printStackTrace();
+                    return;
+                }
+                mConnections.clear();
+                mConnections.addAll(objects);
+                cUserNames.clear();
+                for (int i = 0; i < objects.size(); i++) {
+                    try {
+                        cUserNames.add(objects.get(i).getOtherUser().fetchIfNeeded().getUsername());
+                    } catch (ParseException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void queryUsers() {
+        ParseQuery <ParseUser> query1 = new ParseQuery<ParseUser>(ParseUser.class);
+        query1.whereEqualTo("username", ParseUser.getCurrentUser());
+        ParseQuery <ParseUser> query2 = new ParseQuery<ParseUser>(ParseUser.class);
+        query2.whereNotContainedIn("username", cUserNames);
+
+        List<ParseQuery<ParseUser>> queries = new ArrayList<>();
+        queries.add(query1);
+        queries.add(query2);
+
+        ParseQuery<ParseUser> mainQuery = ParseQuery.or(queries);
+        mainQuery.addDescendingOrder("createdAt");
+        mainQuery.setLimit(20);
+
+        mainQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                mUsers.clear();
+                //for (int i = 0; i < objects.size(); i++) {
+//                    for (int k = 0; k < mConnections.size(); k++) {
+//                        try {
+//                            if (mConnections.get(k).getOtherUser().fetchIfNeeded().getUsername().equals(objects.get(i).fetchIfNeeded().getUsername())) {
+//                               if (!mUsers.contains(objects.get(i))) { mUsers.add(objects.get(i)); }
+//                            }
+//                        } catch (ParseException ee) {
+//                            ee.printStackTrace();
+//                        }
+//                    }
+//                }
+                mUsers.addAll(objects);
+            }
+        });
+    }
+
+    private void checkForConnections() {
+        for (int i = 0; i < mUsers.size(); i++) {
+
         }
     }
 

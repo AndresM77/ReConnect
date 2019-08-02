deeef524824b50e572fe70448dc342d479ec09d6
camsim99 <csimon@cs.hmc.edu>
2019-08-02 09:31:03 -0700

    Clicking on a contact now gives you option to send message or request a meeting

--- app/src/main/java/com/example/reconnect/model/User.java
+++ app/src/main/java/com/example/reconnect/model/User.java
@@ -1,12 +1,93 @@
 package com.example.reconnect.model;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.bumptech.glide.Glide;
+import com.example.reconnect.Activities.HomeActivity;
+import com.example.reconnect.Activities.MessagesActivity;
+import com.example.reconnect.Activities.RequestMeetingActivity;
+import com.example.reconnect.R;
+import com.example.reconnect.fragments.ConversationsFragment;
+import com.parse.FindCallback;
 import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 @ParseClassName("User")
 public class User extends ParseUser {
     public final static String KEY_USER = "username";
+
+    public static void showContactOptions(final Context context, final ParseUser contact) {
+        View messageView = LayoutInflater.from(context).inflate(R.layout.item_contact_alert, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setView(messageView);
+
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+
+        //Configure Text
+        TextView userName = messageView.findViewById(R.id.tvUserName);
+        TextView industry = messageView.findViewById(R.id.tvIndustry);
+        ImageView profileImg = messageView.findViewById(R.id.ivProfileImg);
+        Button btnMessage = messageView.findViewById(R.id.btnMessage);
+        Button btnMeeting = messageView.findViewById(R.id.btnMeeting);
+        try {
+            userName.setText(contact.fetchIfNeeded().getUsername());
+            industry.setText((String) contact.fetchIfNeeded().get("industry"));
+            if (contact.get("profileImg") != null) {
+                ParseFile img = (ParseFile) contact.get("profileImg");
+                Glide.with(context).load(img.getUrl()).circleCrop().into(profileImg);
+            } else {
+                Glide.with(context).load((R.drawable.baseline_account_circle_black_48)).circleCrop().into(profileImg);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, MessagesActivity.class);
+                i.putExtra("contact", contact);
+                context.startActivity(i);
+            }
+        });
+
+        btnMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, RequestMeetingActivity.class);
+                i.putExtra("requesteeId", contact.getObjectId());
+                context.startActivity(i);
+            }
+        });
+
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
+        // Display the dialog
+        alertDialog.show();
+    }
+
+    private void goToConversation(Context context, Conversation conversation) {
+        Intent i = new Intent(context, MessagesActivity.class);
+        i.putExtra("conversation", conversation);
+        context.startActivity(i);
+    }
 
     //Querying our User class
     public static class Query extends ParseQuery<User> {

4b3ec44851d694151721694a20d0d853fcd9323b
Andres Montoya <andres@triplean.com>
2019-08-05 14:14:38 -0700

    Fixed message items to go to appropriate side

--- app/src/main/java/com/example/reconnect/Activities/MessagesActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/MessagesActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -200,6 +201,10 @@ public class MessagesActivity extends AppCompatActivity {
                 queryMessages(conversation);
             }
         });
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvMessages.getContext(),
+                linearLayoutManager.getOrientation());
+        rvMessages.addItemDecoration(dividerItemDecoration);
     }
 
     private void saveMessage() {

e006c9ac5387351d28ac9b7ed749dda14a33a907
Andres Montoya <andres@triplean.com>
2019-08-08 18:05:40 -0700

    Added streaks to new users

--- app/src/main/java/com/example/reconnect/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/reconnect/Activities/RegisterActivity.java
@@ -1,25 +1,16 @@
 package com.example.reconnect.Activities;
 
-import android.content.ContentResolver;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.reconnect.R;
-import com.example.reconnect.model.Connection;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
@@ -109,6 +100,7 @@ public class RegisterActivity extends AppCompatActivity {
         newUser.put("lastName", lastName);
         newUser.put("password", pass);
         newUser.put("industry", industry);
+        newUser.put("streaks", 0);
         newUser.put("location", new ParseGeoPoint(0,0));
         try {
             newUser.signUp();
